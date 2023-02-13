7d90c797d22bc6f8185171423e15d76cf6b46fb7
s-full <stefan.fuller1@gmail.com>
2023-02-13 12:43:19 -0800

    added stuff

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
-public class Main {
-	public static void main(String[] args) {
+import java.util.ArrayList;
+import MajorityElement.java;
 
+public class Main {
+	public static void main(String[] args) {		
 		System.out.println("Hello, world!");
+
+		int[] test1 = new int[]{1,1,1,3,3,4};
+		System.out.println(MajorityElement.MajorityElement(test1)); 
 	}
 }
